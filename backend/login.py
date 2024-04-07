3d369bbf9ee0b770c81472bd283f1378f5c3e2e0
Makarena-211 <mnfomenkov@gmail.com>
2024-04-07 23:33:23 +0300

    modified:   backend/__pycache__/login.cpython-310.pyc 	modified:   backend/login.py

--- backend/login.py
+++ backend/login.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from database_login
+import database_login
 from fastapi import APIRouter
 from config import get_db
 import schema_login
@@ -11,7 +11,7 @@ router = APIRouter(
     tags=['Login']
 )
 
-@router.post("/create_user", status_code=status.HTTP_201_CREATED, response_model=List[schema_login.CreatePerson])
+@router.post("/create_user", status_code=status.HTTP_201_CREATED, response_model=schema_login.CreatePerson)
 def create_user(person_create: schema_login.CreatePerson, db: Session = Depends(get_db)):
     new_person = database_login.Person(**person_create.dict())
     db.add(new_person)
@@ -20,9 +20,9 @@ def create_user(person_create: schema_login.CreatePerson, db: Session = Depends(
     return new_person
 
 
-@router.post("/authenticate_user", response_model=List[schema_login.Person])
-def authenticate_user(person: schema_login.Person, db: Session = Depends(get_db)):
-    person = db.query(person).filterby(person = person.name, password = person.password, role = person.role).first()
-    if not person:
+@router.post("/authenticate_user", response_model=schema_login.Person)
+def authenticate_user(person:schema_login.Person, db: Session = Depends(get_db)):
+    user = db.query(database_login.Person).filter(database_login.Person.name == person.name, database_login.Person.password == person.password, database_login.Person.role == person.role).first()
+    if not user:
         return None
-    return person
+    return user
